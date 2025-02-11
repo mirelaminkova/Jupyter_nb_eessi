539330c94a123ad04d915d48be2cb5552a06421d
mirelaminkova <mirela.minkova@surf.nl>
2025-02-11 11:31:26 +0100

    trial to load modules automatically

--- ansible/jupyter/run.sh
+++ ansible/jupyter/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+source /cvmfs/software.eessi.io/versions/2023.06/init/bash && module load EESSI/2023.06
 
-if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load JupyterNotebook/7.0.2-GCCcore-12.3.0
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load JupyterNotebook/7.0.2-GCCcore-12.3.0 && module load jupyter-server/2.7.2-GCCcore-12.3.0 && module load nodejs/20.9.0-GCCcore-13.2.0
 
 else echo "Don't know which Jupyter notebook  module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}" >&2; exit 1
 fi
